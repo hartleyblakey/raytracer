0fcc08856de9d9572c0ae7e7222b2960bbc9c1ab
Hartley Blakey <hartleyblakey@gmail.com>
2024-11-07 21:45:14 -0800

    initial commit

---
+++ generated/raytracer_bg.wasm.d.ts
@@ -0,0 +1,19 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function main(a: number, b: number): number;
+export function __wbindgen_malloc(a: number, b: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number, d: number): number;
+export const __wbindgen_export_2: WebAssembly.Table;
+export const __wbindgen_export_3: WebAssembly.Table;
+export function closure8_externref_shim(a: number, b: number, c: number): void;
+export function closure13_externref_shim(a: number, b: number, c: number, d: number): void;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h33fc93520654cabd(a: number, b: number): void;
+export function closure364_externref_shim(a: number, b: number, c: number): void;
+export function closure385_externref_shim(a: number, b: number, c: number): void;
+export function _dyn_core__ops__function__FnMut_____Output___R_as_wasm_bindgen__closure__WasmClosure___describe__invoke__h3096d65358b2bfd6(a: number, b: number): void;
+export function closure458_externref_shim(a: number, b: number, c: number): void;
+export function __wbindgen_free(a: number, b: number, c: number): void;
+export function __externref_table_alloc(): number;
+export function __wbindgen_exn_store(a: number): void;
+export function __wbindgen_start(): void;
